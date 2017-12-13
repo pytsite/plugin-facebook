74977423ce1d5e384babc52c5224cec69434f6c6
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:07:08 +0200

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
-"""PytSite Facebook Plugin.
+"""PytSite Facebook Plugin
 """
-# Public API
-from ._api import get_app_id, get_app_secret
-from . import _error as error, _session as session, _widget as widget
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def _init():
+if _plugman.is_installed(__name__):
+    # Public API
+    from ._api import get_app_id, get_app_secret
+    from . import _error as error, _session as session, _widget as widget
+
+
+def plugin_load():
     from pytsite import lang, router
     from plugins import permissions, settings, assetman
     from . import _eh, _settings_form, _controllers
@@ -18,7 +21,7 @@ def _init():
     lang.register_package(__name__)
 
     assetman.register_package(__name__)
-    assetman.t_copy_static(__name__ + '@**')
+    assetman.t_copy_static(__name__)
 
     # Lang globals
     lang.register_global('facebook_admin_settings_url', lambda language, args: settings.form_url('facebook'))
@@ -34,6 +37,3 @@ def _init():
 
     # Event handlers
     router.on_dispatch(_eh.router_dispatch)
-
-
-_init()
